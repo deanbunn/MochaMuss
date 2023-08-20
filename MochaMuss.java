8845e912cc6c80cf5c64b6ed7a7b1c3c8efd2a10
deanbunn <deanbunn@gmail.com>
2023-08-20 14:34:32 -0700

    20230820_1434hrs MussPerson class

--- MochaMuss.java
+++ MochaMuss.java
@@ -1,3 +1,4 @@
+import java.time.LocalDate;
 
 
 public class MochaMuss {
@@ -6,6 +7,39 @@ public class MochaMuss {
 
         System.out.println("Hi Team 5458!");
 
+        //Create Muss People Array
+        MussPerson[] arrMussPeople = new MussPerson[3];
+        arrMussPeople[0] = new MussPerson("Dean Bunn", 2001, 5, 14);
+        arrMussPeople[1] = new MussPerson("Steven Pragg", 2003, 6, 2);
+        arrMussPeople[2] = new MussPerson("Regina White", 2005, 8, 23);
+
+        for(MussPerson mp : arrMussPeople)
+        {
+            System.out.println(mp.getName());
+        }
+
     }
 
+}
+
+class MussPerson
+{
+     private String FullName;
+     private LocalDate Birthday;
+     
+     public MussPerson(String fullName,int bYear, int bMonth, int bDayOfMonth)
+     {
+        FullName = fullName;
+        Birthday = LocalDate.of(bYear,bMonth,bDayOfMonth);
+     }
+
+     public String getName()
+     {
+        return FullName;
+     }
+
+     public LocalDate getBirthDay()
+     {
+        return Birthday;
+     }
 }
